ece7193f8e3c89b63479b97749779b50a712fd53
Filip Jerga <filipjerga@Filips-MBP-2.fritz.box>
2020-03-31 11:07:58 +0200

    Initial Commit

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/index.scss';
+
+function MyApp({ Component, pageProps }) {
+    return <Component {...pageProps} />
+}  
+export default MyApp
